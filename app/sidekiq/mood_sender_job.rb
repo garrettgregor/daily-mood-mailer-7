ddf1700b5d62606f5361c013ba2147b5fe47bb53
Garrett Gregor <garrett.gregor@gmail.com>
2023-08-01 10:51:19 -0600

    finish mailer actions with sidekiq

---
+++ app/sidekiq/mood_sender_job.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+class MoodSenderJob
+  include Sidekiq::Job
+
+  def perform(email, mood)
+    UserNotifierMailer.send_mood_email(email, mood).deliver_now
+  end
+end
